8af57f8044dedffbb1bed016b367d99d8a3f92d5
Gabin An <agb94@kaist.ac.kr>
2024-02-06 14:28:56 +0900

    Get Arxiv RSS Feed

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+import requests
+import xml.etree.ElementTree as ET
+
+CAT = "cs.SE"
+
+def get_rss_feed(category):
+    url = f"https://rss.arxiv.org/rss/{category}"
+    response = requests.get(url=url)
+    status = response.status_code     
+    if status == 200:
+        root = ET.fromstring(response.text)
+        return root
+
 if __name__ == "__main__":
-  with open("crawling_results.md", 'w') as f:
-    f.write("# Nyam Nyam Garibi")
+
+    root = get_rss_feed(CAT)
+
+    text_properties = ["title", "link", "description", "category"]
+    channel = root.find("channel")
+    title = channel.find("title").text
+    lastBuildDate = channel.find("lastBuildDate").text
+
+    ns = {'dc': 'http://purl.org/dc/elements/1.1/'}
+    papers = []
+    for item in channel.iter("item"):
+        properties = {}
+        for prob in ["title", "link", "description", "category"]:
+            properties[prob] = item.find(prob).text
+        properties["authors"] = []
+        for author in item.find("dc:creator", namespaces=ns).iter("a"):
+            properties["authors"].append(author.text)
+        papers.append(properties)
+        
+    with open("crawling_results.md", 'w') as f:
+        f.write(f"# {title}, {lastBuildDate}\n\n")
+
+        for paper in papers:
+            f.write(f"## [{paper['title']}]({paper['link']})\n\n")
+            f.write(", ".join(paper.authors) + "\n\n")
+            f.write(paper.description)
