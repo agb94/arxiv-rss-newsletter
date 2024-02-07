9015676e0a15cbe47f6627131f5ad83424d15f37
Gabin An <agb94@kaist.ac.kr>
2024-02-07 16:24:14 +0900

    Add pdf, author links

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import xml.etree.ElementTree as ET
+# import wordcloud
+# from nltk.corpus import stopwords
 
 CAT = "cs.SE"
 
@@ -11,11 +13,7 @@ def get_rss_feed(category):
         root = ET.fromstring(response.text)
         return root
 
-if __name__ == "__main__":
-
-    root = get_rss_feed(CAT)
-
-    text_properties = ["title", "link", "description", "category"]
+def parse_xml(root):
     channel = root.find("channel")
     title = channel.find("title").text
     lastBuildDate = channel.find("lastBuildDate").text
@@ -28,13 +26,26 @@ if __name__ == "__main__":
             properties[prob] = item.find(prob).text
         properties["authors"] = []
         for author in item.find("dc:creator", namespaces=ns).iter("a"):
-            properties["authors"].append(author.text)
+            properties["authors"].append((author.text, author.attrib["href"]))
         papers.append(properties)
-        
+    return title, lastBuildDate, papers
+
+if __name__ == "__main__":
+
+    root = get_rss_feed(CAT)
+
+    title, date, papers = parse_xml(root)
+
+    # wc = wordcloud.WordCloud(stopwords=stopwords.words('english') + ["using", "based", "code", "software", "system", "model"])
+    # wc.generate(text="\n".join([p["description"] for p in papers]))
+    # print(wc.words_)
+
     with open("crawling_results.md", 'w') as f:
-        f.write(f"# {title}, {lastBuildDate}\n\n")
+        f.write(f"# {title}, {date}\n\n")
 
         for paper in papers:
-            f.write(f"## [{paper['title']}]({paper['link']})\n\n")
-            f.write(", ".join(paper["authors"]) + "\n\n")
+            f.write(f"## [{paper['title']}]({paper['link']}) [[pdf]({paper['link'].replace('abs', 'pdf') + '.pdf'})]\n\n")
+            f.write("Authors: " + ", ".join([f"[{a}]({l})" for a, l in paper["authors"]]) + "\n\n")
             f.write(paper["description"]+ "\n\n")
+
+    
